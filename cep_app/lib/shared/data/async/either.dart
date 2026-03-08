0ceddbf3ea2f47a000de4ca0c3cc6b2047628761
Bruna Drago <bruna_drago@hotmail.com>
2026-03-08 15:23:53 -0300

    [Flutter]Implements Either

---
+++ cep_app/lib/shared/data/async/either.dart
@@ -0,0 +1,16 @@
+sealed class Either<L, R> {}
+
+class Left<L, R> extends Either<L, R> {
+  final L _left;
+
+  const Left(this._left);
+
+  L get left => _left;
+}
+
+class Right<L, R> extends Either<L, R> {
+  final R _right;
+  const Right(this._right);
+
+  R get right => _right;
+}

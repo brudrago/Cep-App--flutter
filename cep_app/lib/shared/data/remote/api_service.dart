b8f4d2ed434b222f30ccb1b13e0a5dc383702d6a
Bruna Drago <bruna_drago@hotmail.com>
2026-03-08 14:55:31 -0300

    [Flutter]Create ApiService base interface

---
+++ cep_app/lib/shared/data/remote/api_service.dart
@@ -0,0 +1,8 @@
+import 'package:cep_app/shared/data/async/either.dart';
+import 'package:cep_app/shared/data/remote/errors/api_exception.dart';
+import 'package:cep_app/shared/data/models/api_base_model.dart';
+import 'package:cep_app/shared/data/models/api_response_model.dart';
+
+abstract interface class ApiService {
+  Future<Either<ApiException, ApiResponseModel<T>>> get<T>(String endpoint, {Map<String, dynamic>? queryParameters});
+}

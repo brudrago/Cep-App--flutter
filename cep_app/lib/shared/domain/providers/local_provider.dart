71efaadf4f567c681492feda20548d9317c29d67
Bruna Drago <bruna_drago@hotmail.com>
2026-03-08 15:57:12 -0300

    [Flutter]Create providers

---
+++ cep_app/lib/shared/domain/providers/local_provider.dart
@@ -0,0 +1,8 @@
+import 'package:cep_app/shared/data/local/local_service/shared_preferences.dart';
+import 'package:cep_app/shared/data/local/local_service/local_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final localServiceProvider = Provider<LocalService>(
+    (ref) => SharedPreferencesService(sharedPreferences: SharedPreferences.instance)
+    );

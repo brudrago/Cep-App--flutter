b3e9918a168d99a4b59f14cb190ce42e115e3c69
Bruna Drago <bruna_drago@hotmail.com>
2026-03-09 20:09:20 -0300

    [Flutter]Improve  SetLocalThemeDataSource

--- cep_app/lib/shared/theme/data/dataSource/set_theme_data_source.dart
+++ cep_app/lib/shared/theme/data/dataSource/set_theme_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:cep_app/shared/data/async/either.dart';
 import 'package:cep_app/shared/data/local/local_service/local_service.dart';
+import 'package:cep_app/shared/theme/data/dataSource/get_theme_local_data_source.dart';
 import 'package:cep_app/shared/theme/errors/theme_local_exception.dart';
 
 abstract interface class SetThemeLocalDataSource {
@@ -9,10 +10,14 @@ abstract interface class SetThemeLocalDataSource {
 final class SetThemeLocalDataSourceImpl implements SetThemeLocalDataSource {
   final LocalService _localService;
 
-  SetThemeLocalDataSourceImpl({this.localService});
+  SetThemeLocalDataSourceImpl({this._localService});
 
   @override
   Future<Either<ThemeException, void>> setIsLightTheme(bool isLightTheme) async {
-    return localService.set<bool>(key: IS_LIGHT_THEME_KEY, value: isLightTheme);
+    final result = await _localService.set(IS_LIGHT_THEME_KEY, isLightTheme);
+    return switch (result) {
+      Left(:final left) => Left(ThemeException(message: left.message)),
+      Right() => Right(null),
+    };
   }
 }

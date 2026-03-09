236a1e50f2a8064c23b529659fd22b46e8f089f3
Bruna Drago <bruna_drago@hotmail.com>
2026-03-09 19:57:34 -0300

    [Flutter]Create GetLocalThemeDataSource

---
+++ cep_app/lib/shared/theme/data/dataSource/get_theme_local_data_source.dart
@@ -0,0 +1,20 @@
+abstract interface class GetThemeLocalDataSource {
+  Future<Either<ThemeException, bool>> getIsLightTheme();
+}
+
+const String IS_LIGHT_THEME_KEY = 'isLightTheme';
+
+final class GetThemeLocalDataSourceImpl implements GetThemeLocalDataSource {
+  final LocalService _localService;
+
+  GetThemeLocalDataSourceImpl({this._localService});
+
+  @override
+  Future<Either<ThemeException, bool>> getIsLightTheme() async {
+    final result = await _localService.get<bool>(IS_LIGHT_THEME_KEY);
+    return result.fold(
+      (l) => Left(ThemeException(message: l.message)),
+      (r) => Right(r ?? true),
+    );
+  }
+}

d3d29c74ed7b3d6a039bca9c5b35e20e42cbb492
Bruna Drago <bruna_drago@hotmail.com>
2026-04-22 21:38:54 -0300

    [Flutter]Create model

---
+++ cep_app/lib/features/cep/data/models/cep_response_model.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:cep_app/shared/domain/entities/cep_response.dart';
+
+class CepResponseModel extends CepResponse {
+  const CepResponseModel({
+    required super.cep,
+    required super.logradouro,
+    required super.complemento,
+    required super.bairro,
+    required super.localidade,
+    required super.uf,
+    required super.estado,
+    required super.regiao,
+    required super.ddd,
+  });
+
+  factory CepResponseModel.fromMap(map) {
+    return CepResponseModel(
+      cep: map['cep'],
+      logradouro: map['logradouro'],
+      complemento: map['complemento'],
+      bairro: map['bairro'],
+      localidade: map['localidade'],
+      uf: map['uf'],
+      estado: map['estado'],
+      regiao: map['regiao'],
+      ddd: map['ddd'],
+    );
+  }
+
+  factory CepResponseModel.fromJson(String json) {
+    return CepResponseModel.fromMap(jsonDecode(json));
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'cep': cep,
+      'logradouro': logradouro,
+      'complemento': complemento,
+      'bairro': bairro,
+      'localidade': localidade,
+      'uf': uf,
+      'estado': estado,
+      'regiao': regiao,
+      'ddd': ddd,
+    };
+  }
+
+  String toJson() => jsonEncode(toMap());
+}
